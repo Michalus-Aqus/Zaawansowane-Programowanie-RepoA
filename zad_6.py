49f193e478ee9e3c98d2a2286235804f4ffdc6ac
Michał <michal.biesek@edu.uekat.pl>
2022-10-16 10:37:05 +0200

    Zadanie 6

--- zad_6.py
+++ zad_6.py
@@ -1,1 +1,6 @@
+def f(a:list,b:list):
+    sum=a+b
+    remove_doubles=[[] if sum[id] in sum[0:id] else [sum[id]] for id in range(len(sum))]
+    return [x**3 for week_element in remove_doubles for x in week_element]
 
+print (f([1,2],[4,1,3]))
